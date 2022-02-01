e5bb814fceb02fa2fee885db1f44b667ee651f2e
Shahin <shahinzaman102@gmail.com>
2022-02-01 18:43:31 +0600

    Navbar hide & show on scroll added (in home-blog-dashboard pages) - Smooth-scroll added (in all pages)

--- public/js/home.js
+++ public/js/home.js
@@ -20,6 +20,18 @@ const createBlog = (blog) => {
     `;
 }
 
+// navbar hide and show on scroll
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.getElementById("navbar").style.top = "0";
+    } else {
+        document.getElementById("navbar").style.top = "-60px";
+    }
+    prevScrollpos = currentScrollPos;
+}
+
 // navigate to search-bar
 document.getElementById("navigate").onclick = function () {
     window.location.hash = "#navigateHere";
