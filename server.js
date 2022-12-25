457f8b99c1b408d2f849edd49c269e5814cdbc38
Shahin Zaman <62700210+hubshahin@users.noreply.github.com>
2022-12-25 21:36:24 +0600

    port updated

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const __dirname = path.dirname(__filename);
 let initial__path = path.join(__dirname, "public");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 app.use(express.static(initial__path));
 // app.use(fileupload());
 
